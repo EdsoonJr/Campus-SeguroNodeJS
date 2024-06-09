f889dac31d1cc27136170cab65c7ebb71116f0fd
Edson Junior <edsonjn2013@gmail.com>
2024-06-09 19:48:24 -0300

    implemente chat base

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-// app.js
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+
+// Adicione as opções de configuração CORS para o socket.io
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+// Configuração do socket.io
+io.on('connection', (socket) => {
+    console.log('Novo cliente conectado');
+
+    socket.on('sendMessage', (message) => {
+        console.log('Mensagem recebida do cliente:', message);
+        io.emit('receiveMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Servidor Rodando na Porta ${port}`);
 });
 
